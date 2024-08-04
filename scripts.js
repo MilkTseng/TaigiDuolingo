c1034ee86553ea1943a97f7743eb1f18c0a5920f
MilkTseng <ryan930703@gmail.com>
2024-08-05 01:15:51 +0800

    change to jQuery

--- scripts.js
+++ scripts.js
@@ -1,81 +1,75 @@
 
+$(document).ready(function() {
+    loadButtons();
+    $("#tailo").on("change", tailoSwitch);
+    $(".buttons").on("click", ".button", function() {
+        playSound($(this));
+    });
+    $(".fa-solid").on("click", function() {
+        playSound($(this));
+    });
+    $(".description-box").on("mouseenter mouseleave", function() {
+        showDD($(this));
+    })
+    $("#show-all").on("change", function() {
+        $(".description-box").find("dd").slideToggle();
+    });
+})
 
-function Alphebet(zhuin, tailo, index){
-    this.zhuin = zhuin;
-    this.tailo = tailo;
-    this.index = index;
+function buttonHTML(zhuin, tailo, index, type) {
+    if(tailo == ""){
+        return `<div class="empty"></div>`;
+    }
+    else {
+        return`
+        <button data-sound="sounds0/${tailo}${index}.mp3" data-zhuin="${zhuin}" data-tailo="${tailo}" class="button ${type}">
+            <span class="big-letter">${zhuin}</span>
+            <span class="small-letter">${tailo}</span>
+        </button>`
+    }
 }
-let consonants = [
-    new Alphebet("ㄅ", "p", 0),
-    new Alphebet("ㄆ", "ph", 0),
-    new Alphebet("ㄇ", "m", 0),
-    new Alphebet("ㆠ", "b", 0),
-    new Alphebet("", ""),
-    new Alphebet("ㄉ", "t", 0),
-    new Alphebet("ㄊ", "th", 0),
-    new Alphebet("ㄋ", "n", 0),
-    new Alphebet("ㄌ", "l", 0),
-    new Alphebet("", ""),
-    new Alphebet("ㄍ", "k", 0),
-    new Alphebet("ㄎ", "kh", 0),
-    new Alphebet("ㄏ", "h", 0),
-    new Alphebet("ㆣ", "g", 0),
-    new Alphebet("ㄫ", "ng", 0),
-    new Alphebet("ㄐ", "ts", 0),
-    new Alphebet("ㄑ", "tsh", 0),
-    new Alphebet("ㄒ", "s", 0),
-    new Alphebet("ㆢ", "j", 0),
-    new Alphebet("", ""),
-    new Alphebet("ㄗ", "ts", 1),
-    new Alphebet("ㄘ" ,"tsh", 1),
-    new Alphebet("ㄙ", "s", 1),
-    new Alphebet("ㆡ", "j", 1),
-    new Alphebet("", ""),
-]
 
-let vowels = [
-    new Alphebet("ㄧ", "i", 0),
-    new Alphebet("ㄨ", "u", 0),
-    new Alphebet("ㆪ", "inn", 0),
-    new Alphebet("ㆫ", "unn", 0),
-    new Alphebet("", ""),
-    new Alphebet("", ""),
-    new Alphebet("ㄚ", "a", 0),
-    new Alphebet("ㆦ", "oo", 0),
-    new Alphebet("ㄜ", "o", 0),
-    new Alphebet("ㆤ", "e", 0),
-    new Alphebet("ㄞ", "ai", 0),
-    new Alphebet("ㄠ", "au", 0),
-    new Alphebet("ㆩ", "ann", 0),
-    new Alphebet("ㆧ", "onn", 0),
-    new Alphebet("", ""),
-    new Alphebet("ㆥ", "enn", 0),
-    new Alphebet("ㆮ", "ainn", 0),
-    new Alphebet("ㆯ", "aunn", 0),
-    new Alphebet("ㆬ", "m", 1),
-    new Alphebet("ㆰ", "am", 0),
-    new Alphebet("ㆱ", "om", 0),
-    new Alphebet("ㄢ", "an", 0),
-    new Alphebet("ㄣ", "n", 1),
-    new Alphebet("", ""),
-    new Alphebet("ㆭ", "ng", 1),
-    new Alphebet("ㄤ", "ang", 0),
-    new Alphebet("ㄥ", "ng", 2),
-    new Alphebet("ㆲ", "ong", 0),
-    new Alphebet("", ""),
-    new Alphebet("", ""),
+function tailoSwitch() {
+    let isTailo = $("#tailo").prop("checked");
+    $(".button").each(function() {
+        let bigLetter = isTailo? $(this).data("tailo"): $(this).data("zhuin");
+        let smallLetter = isTailo? $(this).data("zhuin"): $(this).data("tailo");
+        $(this).find(".big-letter").text(bigLetter);
+        $(this).find(".small-letter").text(smallLetter);
+    });
+}
 
-]
+function playSound(element) {
+    let soundFile = element.data("sound");
+    let audio = new Audio(soundFile);
+    audio.play();
+}
 
-let tones = [
-    new Alphebet("ㄚ", "a", 1),
-    new Alphebet("ㄚ˫", "ā", 7),
-    new Alphebet("ㄚ˪", "à", 3),
-    new Alphebet("ㄚˋ", "á", 2),
-    new Alphebet("ㄚˊ", "â", 5),
-]
+function loadButtons() {
+    $.getJSON("./alphabets.json", function(data) {
+        $.each(data.consonants, function(i, letter)  {
+            let HTML = buttonHTML(letter.zhuin, letter.tailo, letter.index, "consonant");
+            $("#consonants").append(HTML);
+        })
+        $.each(data.vowels, function(i, letter)  {
+            let HTML = buttonHTML(letter.zhuin, letter.tailo, letter.index, "vowel");
+            $("#vowels").append(HTML);
+        })
+        $.each(data.tones, function(i, letter)  {
+            let HTML = buttonHTML(letter.zhuin, letter.tailo, letter.index, "tone");
+            $("#tones").append(HTML);
+        })
+    })
+}
 
-document.addEventListener('DOMContentLoaded', function() {
+function showDD(element) {
+    let isShowAll = $("#show-all").prop("checked");
+    if(!isShowAll){
+        element.find("dd").slideToggle();
+    }
+}
+
+/*document.addEventListener('DOMContentLoaded', function() {
     tailoSwitch();
     loadAlphebet(false);
     buttonSound();
@@ -130,9 +124,9 @@ function loadAlphebet(isTailo){
         }
         else{
             consonantsHtml +=`
-            <button data-sound="sounds0/${consonants[i].tailo}${consonants[i].index}.mp3" class="button consonant">
-                <span class="big-letter">${isTailo? consonants[i].tailo:consonants[i].zhuin}</span>
-                <span class="small-letter">${isTailo? consonants[i].zhuin:consonants[i].tailo}</span>
+            <button data-sound="sounds0/${consonants[i].tailo}${consonants[i].index}.mp3" data-zhuin="${consonants[i].zhuin}" data-tailo="${consonants[i].tailo}" class="button consonant">
+                <span class="big-letter"></span>
+                <span class="small-letter"></span>
             </button>
             `;
         }
@@ -144,9 +138,9 @@ function loadAlphebet(isTailo){
         }
         else{
             vowelsHtml +=`
-            <button data-sound="sounds0/${vowels[i].tailo}${vowels[i].index}.mp3" class="button vowel">
-                <span class="big-letter">${isTailo? vowels[i].tailo:vowels[i].zhuin}</span>
-                <span class="small-letter">${isTailo? vowels[i].zhuin:vowels[i].tailo}</span>
+            <button data-sound="sounds0/${vowels[i].tailo}${vowels[i].index}.mp3" data-zhuin="${vowels[i].zhuin}" data-tailo="${vowels[i].tailo}" class="button vowel">
+                <span class="big-letter"></span>
+                <span class="small-letter"></span>
             </button>
             `;
         }
@@ -155,9 +149,9 @@ function loadAlphebet(isTailo){
     for(i = 0; i < tones.length; i++){
         tonesHtml +=`
         <p class="tone-term">第 ${tones[i].index} 調</p>
-        <button data-sound="sounds0/${tones[i].tailo}${tones[i].index}.mp3" class="button tone">
-            <span class="big-letter">${isTailo? tones[i].tailo:tones[i].zhuin}</span>
-            <span class="small-letter">${isTailo? tones[i].zhuin:tones[i].tailo}</span>
+        <button data-sound="sounds0/${tones[i].tailo}${tones[i].index}.mp3" data-zhuin="${tones[i].zhuin}" data-tailo="${tones[i].tailo}" class="button tone">
+            <span class="big-letter"></span>
+            <span class="small-letter"></span>
         </button>
         `;
         
@@ -169,4 +163,4 @@ function loadAlphebet(isTailo){
     document.querySelector("#vowels").innerHTML = vowelsHtml;
     document.querySelector("#tones").innerHTML = tonesHtml;
     
-}
+}*/
